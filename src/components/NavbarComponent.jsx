201a601e285a6b041c0a8d9c06865e3a33885d26
David <david.torta.olah@gmail.com>
2024-07-02 17:54:42 +0200

    first commit

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -5,7 +5,7 @@ export const NavbarComponent = () => {
     <>
   <nav className="navbar navbar-expand-lg navbar-color">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#">LA ARCILLA EN CASA</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,11 +15,11 @@ export const NavbarComponent = () => {
           <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Carrito</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Categorias
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="#">Action</a></li>
