6a6ab50ad8a1c7154363b0e622b61e33d3769b54
David <david.torta.olah@gmail.com>
2024-07-07 13:37:20 +0200

    authslice

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {ecommerceSlice} from './slices/ecommerce/'
+import { authSlice } from './slices/auth'
 
 /* SI NO FUNCIONA PON ecommerceSlice.reducer */
 
 export const store = configureStore({
   reducer: {
+    auth: authSlice.reducer,
     ecommerce: ecommerceSlice.reducer,
   },
 })
